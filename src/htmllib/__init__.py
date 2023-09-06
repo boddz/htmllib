930971c464137ab2ea0a9db72be955530bef57b2
boddz <boddz.dev@gmail.com>
2023-09-06 22:53:57 +0100

    Import changes & package info updated

--- src/htmllib/__init__.py
+++ src/htmllib/__init__.py
@@ -7,5 +7,28 @@ A module that turns a stream of HTML code into a python representation of it.
 """
 
 
-from .lexer import Lexer, TokenTypes, pretty_print_tokens
-from .parser import Parser
+from sys import version_info
+
+
+assert (version_info.major, version_info.minor) >= (3, 8), f"To import ``{__package__}``, use Python 3.8 or above."
+
+
+__author__ = "boddz"
+__version__ = "1.0.0"
+__license__ = "GNU GPLv3"
+
+
+from .lexer import (
+    Lexer,
+    TokenTypes,
+    pretty_print_tokens
+)
+
+from .parser import (
+    Parser,
+    HTMLDoctypeOrCommNode,
+    HTMLOpeningTagNode,
+    HTMLClosingTagNode,
+    HTMLSelfClosingTagNode,
+    HTMLErrorNode,
+)
