6b6a494a5dc8d7c16a98a9d21788b462240d038a
boddz <boddz.dev@gmail.com>
2023-09-07 23:44:35 +0100

    Updated example in main

--- main.py
+++ main.py
@@ -5,11 +5,16 @@
 From now on, this is the main entery point that needs to be used structure wise for module to work as intended.
 """
 
+from pprint import PrettyPrinter
+
 from src import htmllib
 
 
 if __name__ == "__main__":
-    parser = htmllib.Parser('<html><div id="1"><div id="2">qwerty</div></div></html>')
+    HTMLCODE = "<html lang='en'><body><h1>Hello, World!</h1><p> This is a test </p><p> A good test. </p></body></html>"
+
+    parser = htmllib.Parser(HTMLCODE)
     nodes = parser._parse_tokens_to_node_list()
 
-    print(nodes)
+    pretty = PrettyPrinter(indent=4)
+    pretty.pprint(nodes)
