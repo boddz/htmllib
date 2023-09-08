8e6fbbbe18ba2e4689028b6651e67d59a18475e6
boddz <boddz.dev@gmail.com>
2023-09-08 20:13:49 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 From now on, this is the main entry point that needs to be used structure wise for module to work as intended.
 """
 
+
+from urllib import request
 from pprint import PrettyPrinter
 
 from src import htmllib
@@ -13,7 +15,10 @@ from src import htmllib
 if __name__ == "__main__":
     HTMLCODE = "<html lang='en'><body><h1>Hello, World!</h1><p> This is a test </p> A good test. </p></body></html>"
     HTMLCODE = "<html lang='en' id='123'> <div lang = 'en'    id ='123'/> <p id='123'>"
-    HTMLCODE = "<html id='13' lang='en'> <div lang = 'en'    class ='123'> <p id='123'> </p></html>"
+    HTMLCODE = "<html id='123' lang='en'> <div lang = 'en'    class ='123'> <p id='123'> </p></html>"
+
+    resp = request.urlopen("https://example.com")
+    HTMLCODE = resp.read() if resp.status == 200 else ""
 
     # parser = htmllib.Parser(HTMLCODE)
     # nodes = parser._parse_tokens_to_node_list()
@@ -22,6 +27,7 @@ if __name__ == "__main__":
     # pretty.pprint(nodes)
 
     htmltree = htmllib.HTMLTree(HTMLCODE)
+    # print(htmltree.nodes_list)
 
     # print(htmltree.nodes_list)
     # print(htmltree.doctype_or_comment_nodes)
@@ -30,8 +36,9 @@ if __name__ == "__main__":
     # print(htmltree.self_closing_tag_nodes)
     # print(htmltree.error_nodes)
 
-    # print(htmltree.search_tags_using_attrs({"id": "123", "lang": "en"})[0].inner_html)
-    # print(htmltree.search_tags_using_exact_attrs({"lang": "en", "id": "123"}))
-    # print(htmltree.search_tags_by_attr(("id", "123")))
-    # print(htmltree.search_tags_by_id("123"))
-    print(htmltree.search_tags_by_class("123"))
+    print(htmltree.search_opening_tags_by_name("style"))
+    # print(htmltree.search_opening_tags_by_attrs({"lang": "en", "id": "123"}))
+    # print(htmltree.search_opening_tags_by_exact_attrs({"id": "123", "lang": "en"}))
+    # print(htmltree.search_opening_tags_by_attr(("type", "text/css")))
+    # print(htmltree.search_opening_tags_by_id("123"))
+    # print(htmltree.search_opening_tags_by_class("123"))
