be86b8919a731dd5606b63f9bcafd6e4c9bd7bf8
boddz <boddz.dev@gmail.com>
2023-09-10 01:28:43 +0100

    Corrected type asserts

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -29,11 +29,13 @@ class TestParserMethods(TestCase):
         self.parser_no_valid_id_error = htmllib.Parser("<></>")
         self.parser_never_closed_error = htmllib.Parser("<div id='111' \n\n\nclass='zxc'")
         self.parser_never_closed_error_sc = htmllib.Parser("<img/")
-        self.parser_open_tag_inner_content = htmllib.Parser("<div><div><div>test</div><p>a<p>123</p><p>456</p></p></div>")
+        self.parser_open_tag_inner_content = htmllib.Parser("<div><div><div>test</div><p>a</p><p>456</p></p></div>")
+        self.parser_nothing_to_parse = htmllib.Parser("html>html/>!='sds'")
+        self.parser_not_all_valid = htmllib.Parser("html>html/>!='sds<body id='im_valid'> Yay, I'm valid </body>'")
 
     def test_parser_opening_tag(self) -> None:
         nodes = self.parser_opening_tag._parse_tokens_to_node_list()
-        self.assertEqual(type(nodes[0]), htmllib.HTMLOpeningTagNode)
+        self.assertIsInstance(nodes[0], htmllib.HTMLOpeningTagNode)
         self.assertEqual(nodes[0].tag_name, "html")
         self.assertEqual(nodes[0].attributes, None)
         self.assertEqual(nodes[0].inner_html, None)
@@ -46,7 +48,7 @@ class TestParserMethods(TestCase):
 
     def test_parser_opening_tag_attr(self) -> None:
         nodes = self.parser_opening_tag_attr._parse_tokens_to_node_list()
-        self.assertEqual(type(nodes[0]), htmllib.HTMLOpeningTagNode)
+        self.assertIsInstance(nodes[0], htmllib.HTMLOpeningTagNode)
         self.assertEqual(nodes[0].tag_name, "html")
         self.assertEqual(nodes[0].attributes, {"lang": "en"})
         self.assertEqual(nodes[0].inner_html, None)
@@ -59,7 +61,7 @@ class TestParserMethods(TestCase):
 
     def test_parser_opening_tag_attrs(self) -> None:
         nodes = self.parser_opening_tag_attrs._parse_tokens_to_node_list()
-        self.assertEqual(type(nodes[0]), htmllib.HTMLOpeningTagNode)
+        self.assertIsInstance(nodes[0], htmllib.HTMLOpeningTagNode)
         self.assertEqual(nodes[0].tag_name, "html")
         self.assertEqual(nodes[0].attributes, {"lang": "en", "id": "123</p>sd", "class": "foo"})
         self.assertEqual(nodes[0].inner_html, None)
@@ -72,7 +74,7 @@ class TestParserMethods(TestCase):
 
     def test_parser_closing_tag(self) -> None:
         nodes = self.parser_closing_tag._parse_tokens_to_node_list()
-        self.assertEqual(type(nodes[0]), htmllib.HTMLClosingTagNode)
+        self.assertIsInstance(nodes[0], htmllib.HTMLClosingTagNode)
         self.assertEqual(nodes[0].tag_name, "html")
         self.assertEqual(nodes[0].cursor_start.index, 0)
         self.assertEqual(nodes[0].cursor_start.line, 1)
@@ -83,7 +85,7 @@ class TestParserMethods(TestCase):
 
     def test_parser_self_closing_tag(self) -> None:
         nodes = self.parser_self_closing_tag._parse_tokens_to_node_list()
-        self.assertEqual(type(nodes[0]), htmllib.HTMLSelfClosingTagNode)
+        self.assertIsInstance(nodes[0], htmllib.HTMLSelfClosingTagNode)
         self.assertEqual(nodes[0].tag_name, "img")
         self.assertEqual(nodes[0].attributes, None)
         self.assertEqual(nodes[0].cursor_start.index, 0)
@@ -95,7 +97,7 @@ class TestParserMethods(TestCase):
 
     def test_parser_self_closing_tag_attr(self) -> None:
         nodes = self.parser_self_closing_tag_attr._parse_tokens_to_node_list()
-        self.assertEqual(type(nodes[0]), htmllib.HTMLSelfClosingTagNode)
+        self.assertIsInstance(nodes[0], htmllib.HTMLSelfClosingTagNode)
         self.assertEqual(nodes[0].tag_name, "img")
         self.assertEqual(nodes[0].attributes, {"src": "null"})
         self.assertEqual(nodes[0].cursor_start.index, 0)
@@ -107,7 +109,7 @@ class TestParserMethods(TestCase):
 
     def test_parser_self_closing_tag_attrs(self) -> None:
         nodes = self.parser_self_closing_tag_attrs._parse_tokens_to_node_list()
-        self.assertEqual(type(nodes[0]), htmllib.HTMLSelfClosingTagNode)
+        self.assertIsInstance(nodes[0], htmllib.HTMLSelfClosingTagNode)
         self.assertEqual(nodes[0].tag_name, "img")
         self.assertEqual(nodes[0].attributes, {"src": "null", "alt": "123", "id": "img<img/>id"})
         self.assertEqual(nodes[0].cursor_start.index, 0)
@@ -119,18 +121,18 @@ class TestParserMethods(TestCase):
 
     def test_parser_no_valid_id_error(self) -> None:
         nodes = self.parser_no_valid_id_error._parse_tokens_to_node_list()
-        self.assertEqual(type(nodes[0]), htmllib.HTMLErrorNode)
-        self.assertEqual(type(nodes[0].message), str)
-        self.assertEqual(type(nodes[0].exception), htmllib.NonValidTagIDError)
+        self.assertIsInstance(nodes[0], htmllib.HTMLErrorNode)
+        self.assertIsInstance(nodes[0].message, str)
+        self.assertIsInstance(nodes[0].exception, htmllib.NonValidTagIDError)
         self.assertEqual(nodes[0].cursor_start.index, 0)
         self.assertEqual(nodes[0].cursor_start.line, 1)
         self.assertEqual(nodes[0].cursor_start.col, 1)
         self.assertEqual(nodes[0].cursor_end.index, 1)
         self.assertEqual(nodes[0].cursor_end.line, 1)
         self.assertEqual(nodes[0].cursor_end.col, 2)
-        self.assertEqual(type(nodes[1]), htmllib.HTMLErrorNode)
-        self.assertEqual(type(nodes[1].message), str)
-        self.assertEqual(type(nodes[1].exception), htmllib.NonValidTagIDError)
+        self.assertIsInstance(nodes[1], htmllib.HTMLErrorNode)
+        self.assertIsInstance(nodes[1].message, str)
+        self.assertIsInstance(nodes[1].exception, htmllib.NonValidTagIDError)
         self.assertEqual(nodes[1].cursor_start.index, 2)
         self.assertEqual(nodes[1].cursor_start.line, 1)
         self.assertEqual(nodes[1].cursor_start.col, 3)
@@ -140,9 +142,9 @@ class TestParserMethods(TestCase):
 
     def test_parser_never_closed_error(self) -> None:
         nodes = self.parser_never_closed_error._parse_tokens_to_node_list()
-        self.assertEqual(type(nodes[0]), htmllib.HTMLErrorNode)
-        self.assertEqual(type(nodes[0].message), str)
-        self.assertEqual(type(nodes[0].exception), htmllib.NeverEndedTagError)
+        self.assertIsInstance(nodes[0], htmllib.HTMLErrorNode)
+        self.assertIsInstance(nodes[0].message, str)
+        self.assertIsInstance(nodes[0].exception, htmllib.NeverEndedTagError)
         self.assertEqual(nodes[0].cursor_start.index, 0)
         self.assertEqual(nodes[0].cursor_start.line, 1)
         self.assertEqual(nodes[0].cursor_start.col, 1)
@@ -152,9 +154,9 @@ class TestParserMethods(TestCase):
 
     def test_parser_never_closed_error_self_closing(self) -> None:
         nodes = self.parser_never_closed_error_sc._parse_tokens_to_node_list()
-        self.assertEqual(type(nodes[0]), htmllib.HTMLErrorNode)
-        self.assertEqual(type(nodes[0].message), str)
-        self.assertEqual(type(nodes[0].exception), htmllib.NeverEndedTagError)
+        self.assertIsInstance(nodes[0], htmllib.HTMLErrorNode)
+        self.assertIsInstance(nodes[0].message, str)
+        self.assertIsInstance(nodes[0].exception, htmllib.NeverEndedTagError)
         self.assertEqual(nodes[0].cursor_start.index, 0)
         self.assertEqual(nodes[0].cursor_start.line, 1)
         self.assertEqual(nodes[0].cursor_start.col, 1)
@@ -164,9 +166,16 @@ class TestParserMethods(TestCase):
 
     def test_parser_open_tag_inner_content(self) -> None:
         nodes = self.parser_open_tag_inner_content._parse_tokens_to_node_list()
-        self.assertEqual(type(nodes[0]), htmllib.HTMLOpeningTagNode)
-        self.assertEqual(nodes[1].inner_html, "<div>test</div><p>a<p>123</p><p>456</p></p>")
+        self.assertIsInstance(nodes[0], htmllib.HTMLOpeningTagNode)
+        self.assertEqual(nodes[1].inner_html, "<div>test</div><p>a</p><p>456</p></p>")
         self.assertEqual(nodes[2].inner_html, "test")
-        self.assertEqual(nodes[4].inner_html, "a<p>123</p><p>456</p>")
-        self.assertEqual(nodes[5].inner_html, "123")
-        self.assertEqual(nodes[7].inner_html, "456")
+        self.assertEqual(nodes[4].inner_html, "a")
+        self.assertEqual(nodes[6].inner_html, "456")
+
+    def test_parser_not_all_valid(self) -> None:
+        nodes = self.parser_not_all_valid._parse_tokens_to_node_list()
+        self.assertIsInstance(nodes[0], htmllib.HTMLOpeningTagNode)
+
+    def test_parser_nothing_to_parse(self) -> None:
+        nodes = self.parser_nothing_to_parse._parse_tokens_to_node_list()
+        self.assertEqual(nodes, [])
