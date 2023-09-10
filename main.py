5c335ed16eab641c3e16dea87361ee2d33519e44
boddz <boddz.dev@gmail.com>
2023-09-10 03:53:31 +0100

    Trimmed main.py so it isn't confusing

--- main.py
+++ main.py
@@ -6,44 +6,34 @@ From now on, this is the main entry point that needs to be used structure wise f
 """
 
 
-from urllib import request
-from pprint import PrettyPrinter
-
 from src import htmllib
 
 
 if __name__ == "__main__":
-    # HTMLCODE = "<html lang='en'><body><h1>Hello, World!</h1><p> This is a test </p> A good test. </p></body></html>"
-    # HTMLCODE = "<html lang='en' id='123'> <div lang = 'en'    id ='123'/> <p id='123'>"
-    HTMLCODE = "<html id='123' lang='en'> <div lang = 'en'    class ='123'> <p id='123'> </p></html><img id='123'/>"
+    from src import htmllib
 
-    resp = request.urlopen("https://example.com")
-    HTMLCODE = resp.read() if resp.status == 200 else ""
+    htmltree = htmllib.HTMLTree("""
+        <html lang="en" id="htmlid">
+            <head>
+                <title>HTMLLIB Test Page</title>
+            </head>
+            <body>
+                <section class="main">
+                    <h1>Hello, World!</h1>
+                    <p id="find_me">You found me!</p>
+                    <button type="button" id="pointless">Click Me!</button>
+                </section>
+                <br/>
+                <img src="assets/parse_diagram.png" alt="A diagram of HTMLLib's parse process"/>
+            </body>
+        </html>
+    """)
 
-    # parser = htmllib.Parser(HTMLCODE)
-    # nodes = parser._parse_tokens_to_node_list()
-
-    # pretty = PrettyPrinter(indent=4)
-    # pretty.pprint(nodes)
-
-    htmltree = htmllib.HTMLTree(HTMLCODE)
-
-    # print(htmltree.nodes_list)
-    # print(htmltree.doctype_or_comment_nodes)
-    # print(htmltree.opening_tag_nodes)
-    # print(htmltree.closing_tag_nodes)
-    # print(htmltree.self_closing_tag_nodes)
-    # print(htmltree.error_nodes)
-
-    print(htmltree.search_tags_by_name("title"))
-    # print(htmltree.search_tags_by_attrs({"lang": "en", "id": "123"}))
-    # print(htmltree.search_tags_by_exact_attrs({"id": "123", "lang": "en"}))
-    # print(htmltree.search_tags_by_attr(("type", "text/css")))
-    # print(htmltree.search_tags_by_id("123"))
-    # print(htmltree.search_tags_by_class("123"))
-    # print(htmltree.search_tags_by_name("img", self_closing=True))
-    # print(htmltree.search_tags_by_attrs({"lang": "en", "id": "123"}, self_closing=True))
-    # print(htmltree.search_tags_by_exact_attrs({"id": "123", "lang": "en"}, self_closing=True))
-    # print(htmltree.search_tags_by_attr(("type", "text/css"), self_closing=True))
-    # print(htmltree.search_tags_by_id("123", self_closing=True))
-    # print(htmltree.search_tags_by_class("123", self_closing=True))
+    print(htmltree.search_tags_by_name("html")[0].attributes)
+    print(htmltree.search_tags_by_attrs({"id": "htmlid", "lang": "en"}))
+    print(htmltree.search_tags_by_exact_attrs({"type": "button", "id": "pointless"}))
+    # print(htmltree.search_tags_by_exact_attrs({"id": "pointless", "type": "button"}))  # Nothing due to strict match.
+    print(htmltree.search_tags_by_attr(("type", "button")))
+    print(htmltree.search_tags_by_name("br", self_closing=True))
+    print(htmltree.search_tags_by_id("find_me")[0].inner_html)
+    print(htmltree.search_tags_by_class("main")[0].tag_name)
