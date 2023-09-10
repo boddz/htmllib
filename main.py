ec50baa5cbd1b66175ed971c818239c84a8c2219
boddz <boddz.dev@gmail.com>
2023-09-10 01:27:08 +0100

    Playing around manual testing

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     # HTMLCODE = "<html lang='en' id='123'> <div lang = 'en'    id ='123'/> <p id='123'>"
     HTMLCODE = "<html id='123' lang='en'> <div lang = 'en'    class ='123'> <p id='123'> </p></html><img id='123'/>"
 
-    # resp = request.urlopen("https://example.com")
-    # HTMLCODE = resp.read() if resp.status == 200 else ""
+    resp = request.urlopen("https://example.com")
+    HTMLCODE = resp.read() if resp.status == 200 else ""
 
     # parser = htmllib.Parser(HTMLCODE)
     # nodes = parser._parse_tokens_to_node_list()
@@ -35,15 +35,15 @@ if __name__ == "__main__":
     # print(htmltree.self_closing_tag_nodes)
     # print(htmltree.error_nodes)
 
-    print(htmltree.search_tags_by_name("style"))
-    print(htmltree.search_tags_by_attrs({"lang": "en", "id": "123"}))
-    print(htmltree.search_tags_by_exact_attrs({"id": "123", "lang": "en"}))
-    print(htmltree.search_tags_by_attr(("type", "text/css")))
-    print(htmltree.search_tags_by_id("123"))
-    print(htmltree.search_tags_by_class("123"))
-    print(htmltree.search_tags_by_name("img", self_closing=True))
-    print(htmltree.search_tags_by_attrs({"lang": "en", "id": "123"}, self_closing=True))
-    print(htmltree.search_tags_by_exact_attrs({"id": "123", "lang": "en"}, self_closing=True))
-    print(htmltree.search_tags_by_attr(("type", "text/css"), self_closing=True))
-    print(htmltree.search_tags_by_id("123", self_closing=True))
-    print(htmltree.search_tags_by_class("123", self_closing=True))
+    print(htmltree.search_tags_by_name("title"))
+    # print(htmltree.search_tags_by_attrs({"lang": "en", "id": "123"}))
+    # print(htmltree.search_tags_by_exact_attrs({"id": "123", "lang": "en"}))
+    # print(htmltree.search_tags_by_attr(("type", "text/css")))
+    # print(htmltree.search_tags_by_id("123"))
+    # print(htmltree.search_tags_by_class("123"))
+    # print(htmltree.search_tags_by_name("img", self_closing=True))
+    # print(htmltree.search_tags_by_attrs({"lang": "en", "id": "123"}, self_closing=True))
+    # print(htmltree.search_tags_by_exact_attrs({"id": "123", "lang": "en"}, self_closing=True))
+    # print(htmltree.search_tags_by_attr(("type", "text/css"), self_closing=True))
+    # print(htmltree.search_tags_by_id("123", self_closing=True))
+    # print(htmltree.search_tags_by_class("123", self_closing=True))
