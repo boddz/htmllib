a064cd9e2497683437ba3159817d2bcb7c196e1d
boddz <boddz.dev@gmail.com>
2023-09-10 04:00:54 +0100

    Trimmed main.py so it isn't confusing

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 
-"""
-**FYI**
-From now on, this is the main entry point that needs to be used structure wise for module to work as intended.
-"""
-
 
 from src import htmllib
 
 
 if __name__ == "__main__":
-    from src import htmllib
+    # Manual testing code goes here ::
 
     htmltree = htmllib.HTMLTree("""
         <html lang="en" id="htmlid">
